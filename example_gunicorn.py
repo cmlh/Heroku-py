99af73885f02e6bd16d6280e8e95c52bfee1021d
Christian Heinrich <christian.heinrich@cmlh.id.au>
2015-05-09 12:32:49 +1000

    OAuth Support

--- example_gunicorn.py
+++ example_gunicorn.py
@@ -7,4 +7,18 @@ from sys import argv
 def index():
     return 'Hello!'
 
+@route('/oauth/authorise/basic', method=['GET','POST'])
+def instagram_oauth_redirect():
+    if request.body.len>0:
+        return 'Hello!'
+    
+@route('/instagram/oauth/access/token', method=['GET','POST'])
+def instagram_oauth_access_token():
+    if request.body.len>0:
+        return 'Hello!'
+
+@bottle.route('/oauth/redirect')
+def oauth_redirect():
+    return 'Hello!'
+
 bottle.run(server='gunicorn', host='0.0.0.0', port=argv[1])
