394ca2db90118e87b058fdf9e0120abd75d22e95
Christian Heinrich <christian.heinrich@cmlh.id.au>
2015-05-31 02:35:52 +0100

    Feature Windows Port

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+pip install gunicorn
+pip install bottle
+# Windows
+# pip freeze > .\requirements.txt
 pip freeze > requirements.txt
--- virtualenv.sh
+++ virtualenv.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+# pip install virtualenv
+# virtualenv -p C:\Python27\python.exe venv
 virtualenv -p /usr/bin/python2.7 venv
 source venv/bin/activate
